426ff742acd2f18fc56c39c4a36704c868be1d74
bilge-bzkrt <77617306+bilge-bzkrt@users.noreply.github.com>
2023-06-01 11:50:25 +0300

    Chart added

--- CoreAndFood/Controllers/ChartController.cs
+++ CoreAndFood/Controllers/ChartController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CoreAndFood.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace CoreAndFood.Controllers
 {
@@ -7,6 +9,25 @@ namespace CoreAndFood.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+        public IActionResult VisualizeProductResult()
+        {
+            return Json(ProList());
+        }
+
+        public List<Class1> ProList()
+        {
+            List<Class1> cs = new List<Class1>();
+            cs.Add(new Class1() { proname = "Computer", stock = 150 });
+            cs.Add(new Class1() { proname = "Lcd", stock = 150 });
+            cs.Add(new Class1() { proname = "Usb Disk", stock = 150 });
+            return cs;
         }
     }
 }
