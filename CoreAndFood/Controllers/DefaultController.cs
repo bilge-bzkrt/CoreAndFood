2f6747b0c5e86ba8421b6676abf09536b2cd812d
bilge-bzkrt <77617306+bilge-bzkrt@users.noreply.github.com>
2023-06-02 11:42:31 +0300

    Components are created and categories are listed

---
+++ CoreAndFood/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreAndFood.Controllers
+{
+	public class DefaultController : Controller
+	{
+		[AllowAnonymous]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
